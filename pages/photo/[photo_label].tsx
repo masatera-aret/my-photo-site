6a6ebf957f034595552d7b5a1fb2458fca923117
Masato Terashima <cye_ma_kun245@yahoo.co.jp>
2021-09-20 05:14:19 +0900

    [photo_label].tsxの写真のページ遷移を作成
    まだroute.query.photo_numberがちゃんと修正出来ていない

--- pages/photo/[photo_label].tsx
+++ pages/photo/[photo_label].tsx
@@ -9,44 +9,94 @@ const all_photos = Photos.all_photos;
 
 const PhotoLabel: FC = () => {
   const route = useRouter();
-  const { photo_label, photo_id } = route.query;
+  const { photo_label, photo_id, photo_number } = route.query;
   const [filtered_photo, setFilteredPhotos] = useState<Types.PhotoList[]>();
   const [photo, setPhoto] = useState<Types.PhotoList>();
 
-  function getPhoto(id: number): void {
-    const photo = filtered_photo.find((photo) => photo.id === id);
-    setPhoto(photo);
+  function fetchPhotoById(id: number): Types.PhotoList {
+    return filtered_photo.find((photo) => photo.id === id);
   }
 
-  function getFilteredPhotoFirstId(): number {
-    return filtered_photo[0].id;
+  function fetchIndexByPhotoId(id: number) {
+    return filtered_photo.findIndex((photo) => photo.id === id);
+  }
+
+  function isValidPhotoIndex(index: number): boolean {
+    if (Number.isNaN(index)) return false;
+    const lng = filtered_photo.length;
+    return 0 <= index && index < lng;
+  }
+
+  function isValidPhotoId(id: number): boolean {
+    if (Number.isNaN(id)) return false;
+    return filtered_photo.some((photo) => photo.id === id);
+  }
+
+  function validatePhotoId(id: number): void {
+    console.log("validatePhotoId");
+    if (!isValidPhotoId(id)) route.push(`/photo/${photo_label}?photo_number=0`);
+    setPhoto(fetchPhotoById(id));
+  }
+
+  function validatePhotoIndex(index: number): void {
+    console.log("validatePhotoIndex");
+    if (!isValidPhotoIndex(index)) {
+      route.push(`/photo/${photo_label}?photo_number=0`);
+    }
+    setPhoto(filtered_photo[index]);
+  }
+
+  function prevPhoto() {
+    const index = fetchIndexByPhotoId(photo.id);
+    const last_photo = filtered_photo.length - 1;
+    const prev_photo = index - 1;
+    if (index === 0) {
+      return route.push(`/photo/${photo_label}?photo_number=${last_photo}`);
+    }
+    route.push(`/photo/${photo_label}?photo_number=${prev_photo}`);
+  }
+
+  function nextPhoto() {
+    const index = fetchIndexByPhotoId(photo.id);
+    const last_photo = filtered_photo.length - 1;
+    if (index === last_photo)
+      route.push(`/photo/${photo_label}?photo_number=0`);
+    route.push(`/photo/${photo_label}?photo_number=${index + 1}`);
   }
 
   useEffect(() => {
-    const filtered_photo = all_photos.filter((photo) => {
+    const photos = all_photos.filter((photo) => {
       return photo.label.toLocaleLowerCase() === photo_label;
     });
-    setFilteredPhotos(filtered_photo);
+    setFilteredPhotos(photos);
   }, []);
 
   useEffect(() => {
     if (filtered_photo === undefined) return;
-    if (!filtered_photo.length) {
-      route.replace(`/`);
-      return;
-    }
-    if (!photo_id) return getPhoto(getFilteredPhotoFirstId());
-    getPhoto(Number(photo_id));
+    if (!filtered_photo.length) route.replace(`/`);
+    if (photo_id) return validatePhotoId(Number(photo_id));
+    if (photo_number) return validatePhotoIndex(Number(photo_number));
+    if (!photo_id && !photo_number) return setPhoto(filtered_photo[0]);
   }, [filtered_photo]);
 
   return (
     <>
+      <h1>{photo_number}</h1>
       {photo && (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
+          className={`relative`}
         >
+          <span
+            className={`absolute top-0 left-0 h-full w-1/2 cursor-pointer z-10`}
+            onClick={prevPhoto}
+          ></span>
+          <span
+            className={`absolute top-0 right-0 h-full w-1/2 cursor-pointer z-10`}
+            onClick={nextPhoto}
+          ></span>
           <Image
             src={photo.src}
             width={photo.src.width}
@@ -58,4 +108,4 @@ const PhotoLabel: FC = () => {
   );
 };
 
-export default PhotoLabel;
+export default memo(PhotoLabel);
