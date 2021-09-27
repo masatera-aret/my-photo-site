9e5c798a42bb556e7d3c211f364fe9680bdb35c2
Masato Terashima <cye_ma_kun245@yahoo.co.jp>
2021-09-28 00:24:02 +0900

    もう一度リファクタリングテスト

--- assets/ts/images.ts
+++ assets/ts/images.ts
@@ -1,66 +1,68 @@
 import * as Types from "./types/types"
 
-import Egypt1 from "../../public/images/Egypt/_MG_5531.jpg";
-import Egypt2 from "../../public/images/Egypt/_MG_5721.jpg";
-import Egypt3 from "../../public/images/Egypt/_MG_6327.jpg";
-import Egypt4 from "../../public/images/Egypt/_MG_6324.jpg";
-import Egypt5 from "../../public/images/Egypt/_MG_6357.jpg";
-import Egypt6 from "../../public/images/Egypt/_MG_6388.jpg";
-import Egypt7 from "../../public/images/Egypt/_MG_6621.jpg";
+import Egypt1 from "@/public/images/Egypt/_MG_5531.jpg";
+import Egypt2 from "@/public/images/Egypt/_MG_5721.jpg";
+import Egypt3 from "@/public/images/Egypt/_MG_6327.jpg";
+import Egypt4 from "@/public/images/Egypt/_MG_6324.jpg";
+import Egypt5 from "@/public/images/Egypt/_MG_6357.jpg";
+import Egypt6 from "@/public/images/Egypt/_MG_6388.jpg";
+import Egypt7 from "@/public/images/Egypt/_MG_6621.jpg";
 
-import Turkey1 from "../../public/images/Turkey/_MG_3775.jpg";
-import Turkey2 from "../../public/images/Turkey/_MG_3813.jpg";
-import Turkey3 from "../../public/images/Turkey/_MG_3841.jpg";
-import Turkey4 from "../../public/images/Turkey/_MG_4037.jpg";
-import Turkey5 from "../../public/images/Turkey/_MG_4095.jpg";
-import Turkey6 from "../../public/images/Turkey/_MG_4135.jpg";
-import Turkey7 from "../../public/images/Turkey/_MG_4264.jpg";
-import Turkey8 from "../../public/images/Turkey/_MG_4296.jpg";
-import Turkey9 from "../../public/images/Turkey/_MG_4308.jpg";
-import Turkey10 from "../../public/images/Turkey/_MG_4321.jpg";
-import Turkey11 from "../../public/images/Turkey/_MG_4810.jpg";
-import Turkey12 from "../../public/images/Turkey/_MG_4846.jpg";
-import Turkey13 from "../../public/images/Turkey/_MG_4907.jpg";
-import Turkey14 from "../../public/images/Turkey/_MG_5266.jpg";
-import Turkey15 from "../../public/images/Turkey/_MG_5272.jpg";
-import Turkey16 from "../../public/images/Turkey/_MG_5330.jpg";
-import Turkey17 from "../../public/images/Turkey/_MG_3836-1.jpg";
-import Turkey18 from "../../public/images/Turkey/_MG_4663-1.jpg";
+import Turkey1 from "@/public/images/Turkey/_MG_3775.jpg";
+import Turkey2 from "@/public/images/Turkey/_MG_3813.jpg";
+import Turkey3 from "@/public/images/Turkey/_MG_3841.jpg";
+import Turkey4 from "@/public/images/Turkey/_MG_4037.jpg";
+import Turkey5 from "@/public/images/Turkey/_MG_4095.jpg";
+import Turkey6 from "@/public/images/Turkey/_MG_4135.jpg";
+import Turkey7 from "@/public/images/Turkey/_MG_4264.jpg";
+import Turkey8 from "@/public/images/Turkey/_MG_4296.jpg";
+import Turkey9 from "@/public/images/Turkey/_MG_4308.jpg";
+import Turkey10 from "@/public/images/Turkey/_MG_4321.jpg";
+import Turkey11 from "@/public/images/Turkey/_MG_4810.jpg";
+import Turkey12 from "@/public/images/Turkey/_MG_4846.jpg";
+import Turkey13 from "@/public/images/Turkey/_MG_4907.jpg";
+import Turkey14 from "@/public/images/Turkey/_MG_5266.jpg";
+import Turkey15 from "@/public/images/Turkey/_MG_5272.jpg";
+import Turkey16 from "@/public/images/Turkey/_MG_5330.jpg";
+import Turkey17 from "@/public/images/Turkey/_MG_3836-1.jpg";
+import Turkey18 from "@/public/images/Turkey/_MG_4663-1.jpg";
 
-import Jordan1 from "../../public/images/Jordan/_MG_6692.jpg";
-import Jordan2 from "../../public/images/Jordan/_MG_6711.jpg";
-import Jordan3 from "../../public/images/Jordan/_MG_6994.jpg";
-import Jordan4 from "../../public/images/Jordan/_MG_7025.jpg";
-import Jordan5 from "../../public/images/Jordan/_MG_7042.jpg";
-import Jordan6 from "../../public/images/Jordan/_MG_7082.jpg";
-import Jordan7 from "../../public/images/Jordan/_MG_7159.jpg";
+import Jordan1 from "@/public/images/Jordan/_MG_6692.jpg";
+import Jordan2 from "@/public/images/Jordan/_MG_6711.jpg";
+import Jordan3 from "@/public/images/Jordan/_MG_6994.jpg";
+import Jordan4 from "@/public/images/Jordan/_MG_7025.jpg";
+import Jordan5 from "@/public/images/Jordan/_MG_7042.jpg";
+import Jordan6 from "@/public/images/Jordan/_MG_7082.jpg";
+import Jordan7 from "@/public/images/Jordan/_MG_7159.jpg";
 
-import Morocco1 from "../../public/images/Morocco/_MG_1862.jpg";
-import Morocco2 from "../../public/images/Morocco/_MG_1864.jpg";
-import Morocco3 from "../../public/images/Morocco/_MG_1914.jpg";
-import Morocco4 from "../../public/images/Morocco/_MG_1932.jpg";
-import Morocco5 from "../../public/images/Morocco/_MG_1938.jpg";
-import Morocco6 from "../../public/images/Morocco/_MG_1966.jpg";
-import Morocco7 from "../../public/images/Morocco/_MG_2096.jpg";
-import Morocco8 from "../../public/images/Morocco/_MG_2292.jpg";
-import Morocco9 from "../../public/images/Morocco/_MG_2374.jpg";
-import Morocco10 from "../../public/images/Morocco/_MG_2472.jpg";
-import Morocco11 from "../../public/images/Morocco/_MG_2612.jpg";
+import Morocco1 from "@/public/images/Morocco/_MG_1862.jpg";
+import Morocco2 from "@/public/images/Morocco/_MG_1864.jpg";
+import Morocco3 from "@/public/images/Morocco/_MG_1914.jpg";
+import Morocco4 from "@/public/images/Morocco/_MG_1932.jpg";
+import Morocco5 from "@/public/images/Morocco/_MG_1938.jpg";
+import Morocco6 from "@/public/images/Morocco/_MG_1966.jpg";
+import Morocco7 from "@/public/images/Morocco/_MG_2096.jpg";
+import Morocco8 from "@/public/images/Morocco/_MG_2292.jpg";
+import Morocco9 from "@/public/images/Morocco/_MG_2374.jpg";
+import Morocco10 from "@/public/images/Morocco/_MG_2472.jpg";
+import Morocco11 from "@/public/images/Morocco/_MG_2612.jpg";
 
-import Flower1 from "../../public/images/Flower/_MG_2735.jpg";
-import Flower2 from "../../public/images/Flower/img_2.jpg";
-import Flower3 from "../../public/images/Flower/img_3.jpg";
-import Flower4 from "../../public/images/Flower/img_5.jpg";
-import Flower5 from "../../public/images/Flower/img_6.jpg";
-import Flower6 from "../../public/images/Flower/img_7.jpg";
-import Flower7 from "../../public/images/Flower/img_10.jpg";
-import Flower8 from "../../public/images/Flower/img_11.jpg";
-import Flower9 from "../../public/images/Flower/img_12.jpg";
-import Flower10 from "../../public/images/Flower/img_13.jpg";
-import Flower11 from "../../public/images/Flower/img_14.jpg";
-import Flower12 from "../../public/images/Flower/img_15.jpg";
+import Flower1 from "@/public/images/Flower/_MG_2735.jpg";
+import Flower2 from "@/public/images/Flower/img_2.jpg";
+import Flower3 from "@/public/images/Flower/img_3.jpg";
+import Flower4 from "@/public/images/Flower/img_5.jpg";
+import Flower5 from "@/public/images/Flower/img_6.jpg";
+import Flower6 from "@/public/images/Flower/img_7.jpg";
+import Flower7 from "@/public/images/Flower/img_10.jpg";
+import Flower8 from "@/public/images/Flower/img_11.jpg";
+import Flower9 from "@/public/images/Flower/img_12.jpg";
+import Flower10 from "@/public/images/Flower/img_13.jpg";
+import Flower11 from "@/public/images/Flower/img_14.jpg";
+import Flower12 from "@/public/images/Flower/img_15.jpg";
 
-import Landscape1 from "../../public/images/Landscape/_MG_5808.jpg";
+import Landscape1 from "@/public/images/Landscape/_MG_5808.jpg";
+import Landscape2 from "@/public/images/Landscape/_MG_5888.jpg";
+import Landscape3 from "@/public/images/Landscape/_MG_8672.jpg";
 
 export const all_photos: Types.PhotoList[] = [
   //エジプト
@@ -109,6 +111,7 @@ export const all_photos: Types.PhotoList[] = [
   { id: 308, src: Morocco8, label: "Morocco", alt: "モロッコの写真" },
   { id: 309, src: Morocco9, label: "Morocco", alt: "モロッコの写真" },
   { id: 310, src: Morocco10, label: "Morocco", alt: "モロッコの写真" },
+  { id: 311, src: Morocco11, label: "Morocco", alt: "モロッコの写真" },
   //お花
   { id: 401, src: Flower1, label: "Flower", alt: "お花の写真" },
   { id: 402, src: Flower2, label: "Flower", alt: "お花の写真" },
@@ -124,6 +127,8 @@ export const all_photos: Types.PhotoList[] = [
   { id: 412, src: Flower12, label: "Flower", alt: "お花の写真" },
   //Landscape
   { id: 500, src: Landscape1, label: "Landscape", alt: "風景の写真" },
+  { id: 501, src: Landscape2, label: "Landscape", alt: "風景の写真" },
+  { id: 502, src: Landscape3, label: "Landscape", alt: "風景の写真" },
 ]
 
 export const top_view_photos: Types.PhotoList[] = [
