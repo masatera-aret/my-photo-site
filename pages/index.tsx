3f63ed52328f26b365631631c162d8d9a2f806a2
Masato Terashima <cye_ma_kun245@yahoo.co.jp>
2021-10-14 03:07:09 +0900

    まだ途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: React.FC = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const isModalActive = useSelector((state: StoreState) => state.isModalActive);
   const siteTitle = useSelector((state: StoreState) => state.siteTitle);
+  const images = useSelector((state: StoreState) => state.images);
   const [news] = useState(newsData !== undefined && JSON.parse(newsData));
 
   return (
