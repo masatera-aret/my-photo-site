0ba2efef2414dc801875a4849ff29a9c62f5df5d
Masato Terashima <cye_ma_kun245@yahoo.co.jp>
2021-10-23 13:16:10 +0900

    テストでpushします

--- components/photo_label/ViewPhotoElment.tsx
+++ components/photo_label/ViewPhotoElment.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Images from "next/image";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 import Loading from "../Loading";
 import { ImageType } from "@/pages/index";
@@ -9,6 +10,10 @@ type Params = {
 };
 
 const ViewPhotoElment: React.FC<Params> = ({ imageRef }) => {
+  const router = useRouter();
+  const [isPhotoVertical, setIsPhotoVertical] = useState<boolean>();
+  const { photo_label, id } = router.query;
+
   const [isImageLoading, setImageLoad] = useState(true);
 
   function photoLoaded() {
@@ -16,12 +21,19 @@ const ViewPhotoElment: React.FC<Params> = ({ imageRef }) => {
   }
 
   function prevPhoto() {
-    console.log("prevPhoto");
+    const prev = Number(id) - 1;
+    router.push(`/photo/${photo_label}?id=${prev}`);
   }
 
   function nextPhoto() {
-    console.log("nextPhoto");
+    const next = Number(id) + 1;
+    router.push(`/photo/${photo_label}?id=${next}`);
   }
+
+  useEffect(() => {
+    if (!imageRef) return;
+    setIsPhotoVertical(imageRef.width < imageRef.height);
+  }, [imageRef]);
 
   return (
     <>
@@ -29,7 +41,9 @@ const ViewPhotoElment: React.FC<Params> = ({ imageRef }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className={`relative inline-block leading-3`}
+        className={`relative inline-block leading-3 ${
+          isPhotoVertical ? `w-3/4 max-w-[600px]` : `max-w-[950px]`
+        }`}
       >
         <span
           className={`absolute top-0 left-0 h-full w-1/2 cursor-pointer z-10`}
@@ -39,7 +53,7 @@ const ViewPhotoElment: React.FC<Params> = ({ imageRef }) => {
           className={`absolute top-0 right-0 h-full w-1/2 cursor-pointer z-10`}
           onClick={nextPhoto}
         ></span>
-        <Images
+        <Image
           src={imageRef.url}
           alt={``}
           width={imageRef.width}
--- pages/photo/[photo_label].tsx
+++ pages/photo/[photo_label].tsx
@@ -3,59 +3,33 @@ import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { StoreState } from "@/store/index";
 import Head from "next/head";
-import * as Types from "@/assets/ts/types/types";
 import ViewPhotoElment from "@/components/photo_label/ViewPhotoElment";
 import axios from "axios";
 import { ImageType } from "@/pages/index";
 
 type ParamsType = {
   images: ImageType[];
+  // photo: ImageType;
 };
 
 const PhotoLabel: React.FC<ParamsType> = ({ images }) => {
+  const sortdImages = images.sort((a, b) => {
+    if (a.id > b.id) return -1;
+    if (a.id < b.id) return 1;
+    return 0;
+  });
   const route = useRouter();
   const { photo_label, id, num } = route.query;
-  const testPhotoRef = images.find((e) => e.id === `${photo_label}_${id}`);
+  const [testPhotoRef, setTestPhotoRef] = useState<ImageType>();
+
+  useEffect(() => {
+    const img = images.find((e) => e.id === `${photo_label}_${id}`);
+    setTestPhotoRef(img);
+  }, [id]);
 
   const locationTitle =
     typeof photo_label === "string" && photo_label.toUpperCase();
   const siteTitle = useSelector((state: StoreState) => state.siteTitle);
-  const [filtered_photo, setFilteredPhotos] = useState<Types.PhotoList[]>();
-  const [current_photo, setCurrentPhoto] = useState<Types.PhotoList>();
-
-  function fetchPhotoById(id: number): Types.PhotoList {
-    if (!filtered_photo) return;
-    return filtered_photo.find((photo) => photo.id === id);
-  }
-
-  function isValidPhotoIndex(index: number): boolean {
-    if (Number.isNaN(index)) return false;
-    if (filtered_photo) {
-      const lng = filtered_photo.length;
-      return 0 <= index && index < lng;
-    }
-    return false;
-  }
-
-  function isValidPhotoId(id: number): boolean {
-    if (Number.isNaN(id)) return false;
-
-    return filtered_photo.some((photo) => photo.id === id);
-  }
-
-  function validatePhotoId(id: number): void {
-    if (!isValidPhotoId(id)) route.push(`/photo/${photo_label}?num=1`);
-    setCurrentPhoto(fetchPhotoById(id));
-  }
-
-  function validatePhotoIndex(index: number): void {
-    const inner_index = index - 1;
-    if (!isValidPhotoIndex(inner_index)) {
-      route.push(`/photo/${photo_label}?num=1`);
-      return;
-    }
-    setCurrentPhoto(filtered_photo[inner_index]);
-  }
 
   return (
     <>
